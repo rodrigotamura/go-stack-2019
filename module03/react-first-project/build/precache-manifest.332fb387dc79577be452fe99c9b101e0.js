a83394246eefadb64de7e9f195c1683d93530d82
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-03 11:30:00 -0300

    Sending builded app

---
+++ module03/react-first-project/build/precache-manifest.332fb387dc79577be452fe99c9b101e0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117a1fe85f722819e35f43dda8a44c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d4409e37a7ed4fd681f",
+    "url": "/static/js/2.106817f6.chunk.js"
+  },
+  {
+    "revision": "72ce5d427b8700e08770",
+    "url": "/static/js/main.98e9432d.chunk.js"
+  },
+  {
+    "revision": "a38e5f2a8363cf28bd25",
+    "url": "/static/js/runtime~main.83c63fe4.js"
+  }
+]);
