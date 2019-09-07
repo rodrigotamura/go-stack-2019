836e575c4f54062d1ffa91003fb59acb71a0e53a
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-07 01:06:26 -0300

    Listing starred repos from user, and another nice feats.

--- module03/react_native_project/src/pages/User/index.js
+++ module03/react_native_project/src/pages/User/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
 import api from '../../services/api';
+
+import { Container, Header, Avatar, Name, Bio,
+        Stars, Starred, OwnerAvatar, Info, Title, Author } from './styles.js';
 
 // import { Container } from './styles';
 
@@ -37,23 +40,56 @@ export default class User extends Component {
   }
 
   state = {
-    stars: []
+    stars: [],
+    loading: false
   }
 
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
-    const { stars } = this.state;
+    const { stars, loading } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
 
     return (
-      <View />
+      <Container>
+        <Header>
+          <Avatar source={ { uri: user.avatar } } />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        { loading ? (<ActivityIndicator color="#7159c1" />)
+        :
+        (
+          <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          // onEndReached={this.handleLoadMore} // loads when end reach
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )} />
+        )}
+
+      </Container>
     );
   }
 }
+
+
+// we can implement also WebView, when user click on some repo will open a integrated browser.
