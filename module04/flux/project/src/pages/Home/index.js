3dc5ff4cb882b6e7ef8ed3159dd71cf71c72591f
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-11 23:04:54 -0300

    GH-PAges

--- module04/flux/project/src/pages/Home/index.js
+++ module04/flux/project/src/pages/Home/index.js
@@ -52,6 +52,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
     return (
       <ProductList>
         { products.map(product => (
@@ -61,7 +62,7 @@ class Home extends Component {
             <span>{product.priceFormatted}</span>
             <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                <MdAddShoppingCart size={16} color="#FFF" /> {amount[product.id] || 0}
               </div>
 
               <span>Add to cart</span>
@@ -73,7 +74,14 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
