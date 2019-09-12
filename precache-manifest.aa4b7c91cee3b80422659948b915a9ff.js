000dfa4b2ec618167e80e82d7ee669b0825d7638
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-09-11 23:04:11 -0300

    Updates

---
+++ precache-manifest.aa4b7c91cee3b80422659948b915a9ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b4225ee501fe1a7c09f0062dbdcbaf",
+    "url": "/go-stack-2019/module04/flux/project/index.html"
+  },
+  {
+    "revision": "1c6b11735e150ae874bd",
+    "url": "/go-stack-2019/module04/flux/project/static/js/2.6967b875.chunk.js"
+  },
+  {
+    "revision": "bc94bb09a9ce81cd445d",
+    "url": "/go-stack-2019/module04/flux/project/static/js/main.c0bdc747.chunk.js"
+  },
+  {
+    "revision": "5e3e69bd29249b3dbe74",
+    "url": "/go-stack-2019/module04/flux/project/static/js/runtime~main.39d8aa66.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/go-stack-2019/module04/flux/project/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/go-stack-2019/module04/flux/project/static/media/logo.879557c5.svg"
+  }
+]);
